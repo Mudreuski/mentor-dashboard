2685f69e2a0f20ddecc8da586e5a3fabede62c1e
Mudreuski <bahus1994@mail.ru>
2019-02-27 04:16:11 +0300

    feat: add gh-pages

---
+++ precache-manifest.1053face1026740e1134e61ba81baab1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5610ff344fadf3b83633",
+    "url": "/static/js/main.5610ff34.chunk.js"
+  },
+  {
+    "revision": "46a41ebe3e3fa416ff9f",
+    "url": "/static/js/2.46a41ebe.chunk.js"
+  },
+  {
+    "revision": "5610ff344fadf3b83633",
+    "url": "/static/css/main.0436da4d.chunk.css"
+  },
+  {
+    "revision": "be419688b32029c0082319b80a2ff96a",
+    "url": "/index.html"
+  }
+];
